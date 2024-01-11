d97247f11403dfe59dae7b57a2a45b939c16af03
Maksym Charuk <maxcharuk@gmail.com>
2024-01-11 17:09:38 -0500

    Config server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'http://159.203.96.116/api',
+  admin: {
+    url: 'http://159.203.96.116/dashboard',
+    auth: {
+      secret: "enter-your-jwt-here",
+    },
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
