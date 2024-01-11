6b66275de07a029d0de24aabf887d5180959a6fd
Maksym Charuk <maxcharuk@gmail.com>
2024-01-11 17:22:46 -0500

    Fix server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://159.203.96.116',
+  url: 'http://159.203.96.116/api',
   admin: {
     url: 'http://159.203.96.116/dashboard',
     auth: {
