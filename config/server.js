2c9353ea2e03405600dae2c3c624bcc69595d329
Maksym Charuk <maxcharuk@gmail.com>
2024-01-11 21:40:01 -0500

    Fix server,js

--- config/server.js
+++ config/server.js
@@ -2,9 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('URL', 'http://localhost:1337'),
-  // 'http://159.203.96.116/api'
   admin: {
-    // 'http://159.203.96.116/dashboard'
     url: env('ADMIN_URL', 'http://localhost:1337/dashboard'),
     auth: {
       secret: env('JWT_SECRET', undefined)
